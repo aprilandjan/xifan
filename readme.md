c3dea832d085c809193b0f76ce9afc3ef80641ed
叶剑龙 <yejianlong@meiriq.com>
2017-11-27 00:34:36 +0800

    fix: restruct demo program

---
+++ readme.md
@@ -0,0 +1,3 @@
+# xifan
+
+> `Xifan` is the name of my cat =w=
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
 import Stage from './core/display/Stage'
+import Container from './core/display/Container'
 import Ticker from './core/ticker/Ticker'
 import Segment from './Segment'
 import Point from './core/utils/Point';
 
-const stg:Stage = new Stage('root')
-console.log(stg)
+const stage:Stage = new Stage('root')
+console.log(stage)
 
-stg.x = 100
-stg.y = 200
-stg.rotation = 30
+const main: Container = new Container()
+main.x = stage.width / 2
+main.y = stage.height / 2
+stage.addChild(main)
 
 const seg1 = new Segment(200, 40)
-stg.addChild(seg1)
-
-// seg1.x = 300
-// seg1.y = 100
+main.addChild(seg1)
 
 const seg2 = new Segment(200, 40)
-stg.addChild(seg2)
+main.addChild(seg2)
 
 Ticker.register(() => {
   seg1.rotation += 1
@@ -25,7 +24,7 @@ Ticker.register(() => {
   seg2.x = pin1.x
   seg2.y = pin1.y
 
-  // seg2.rotation -= 1
+  seg2.rotation -= 0.5
 
-  stg.update()
+  stage.update()
 })
